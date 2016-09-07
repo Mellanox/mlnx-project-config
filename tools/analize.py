76a688a10f28b1d9fd39a73f4d0106ce43d47c4a
Lenny Verkhovsky <lennyb@mellanox.com>
2016-09-07 15:18:32 +0000

    added git clone failure check to error analizer
    Change-Id: Ia8edf7752ea1c152d35785e0714c08fa861f3420

--- tools/analize.py
+++ tools/analize.py
@@ -5,7 +5,7 @@ import tempfile
 import smtplib
 from email.mime.text import MIMEText
 import sys
-
+import os
 
 known_issues = { '1': { 'file': 'stack.sh.log',
                         'pattern': "Unable to add physical volume '/dev/loop0' to volume group 'stack-volumes-lvmdriver-1'",
@@ -18,6 +18,10 @@ known_issues = { '1': { 'file': 'stack.sh.log',
                  '3': { 'file': 'stack.sh.log',
                         'pattern': "Unable to look up git.openstack.org",
                         'msg': "PLease rerun the job again"
+                      },
+                 '4': { 'file': 'stack.sh.log',
+                        'pattern': "git call failed",
+                        'msg': "PLease rerun the job again"
                       }
                }
 
@@ -27,16 +31,18 @@ report = { "mail": "lennyb@mellanox.com" }
 def send_email(rm):
     fp = tempfile.NamedTemporaryFile(delete=True)
     fp.write('RM %s is back again\n' % rm)
-    fp.write(known_issues[rm]['msg'])
+    fp.write("%s\n" % known_issues[rm]['msg'])
+    fp.write("1 %s\n" % os.environ)
+    fp.write("cloudx-16-01:/home/CI/scripts/rerun_jobs.py -p %s -j %s\n" % (os.environ.get('JOB_NAME',""), os.environ.get('BUILD_NUMBER',"")))
     fp.seek(0)
     msg = MIMEText(fp.read())
     fp.close()
-    print msg
 
     msg['Subject'] = "CI Failure, %s issue back" % rm
     msg['From'] = 'lennyb@mellanox.com'
     msg['To'] = report['mail']
-
+    print "Sending email to %s" % report["mail"]
+    print msg
     s = smtplib.SMTP('localhost')
     s.sendmail(msg['From'], [msg['From']], msg.as_string())
     s.quit()
