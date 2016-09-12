75ca2bee9f963f10f702413a26834636ccb396d0
Lenny Verkhovsky <lennyb@mellanox.com>
2016-09-12 08:05:03 +0000

    Added RM#861642 check
    Change-Id: Iea5b48448e13aa0af0920b0d02eba577249979d7

--- tools/analize.py
+++ tools/analize.py
@@ -23,20 +23,27 @@ known_issues = { '1': { 'file': 'stack.sh.log',
                         'pattern': "git call failed",
                         'msg': "PLease rerun the job again"
                       },
+                 '861642': { 'file': 'n-cpu.log',
+                        'pattern': "libvirtError: Domain not found: no domain with matching uuid",
+                        'msg': "Open Issue need to debug"
+                      },
                  'stack_failed': { 'file': 'stack.sh.log',
                         'pattern': "exit",
                         'msg': "stack.sh failed. please check"
                       }
+
                }
 
 report = { "mail": "lennyb@mellanox.com" }
 
-
 def send_email(rm):
     fp = tempfile.NamedTemporaryFile(delete=True)
     fp.write('RM %s is back again\n' % rm)
+
+    #http://13.69.151.247/05/357105/5/check-nova/Nova-ML2-Sriov/46b5418/env/environment.txt.gz
+    fp.write('More details in http://%s/%s\n' % (os.environ.get('EXT_SERVER','') ,os.environ.get('LOG_PATH','')))
     fp.write("%s\n" % known_issues[rm]['msg'])
-    #fp.write("1 %s\n" % os.environ)
+    fp.write("\n\n\n %s\n" % os.environ)
     fp.write("cloudx-16-01:/home/CI/scripts/rerun_jobs.py -p %s -j %s\n" % (os.environ.get('JOB_NAME',""), os.environ.get('BUILD_NUMBER',"")))
     fp.seek(0)
     msg = MIMEText(fp.read())
