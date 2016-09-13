8dfee16f7fd18c1f261826c06674de062da86616
Lenny Verkhovsky <lennyb@mellanox.com>
2016-09-13 06:33:51 +0000

    added moshele@mellanox.com email
    Change-Id: I0561ebbc5c201b16642ad96cd15363d3428d8c87

--- tools/analize.py
+++ tools/analize.py
@@ -34,7 +34,7 @@ known_issues = { '1': { 'file': 'stack.sh.log',
 
                }
 
-report = { "mail": "lennyb@mellanox.com" }
+report = { "mail": "lennyb@mellanox.com,moshele@mellanox.com" }
 
 def send_email(rm):
     fp = tempfile.NamedTemporaryFile(delete=True)
@@ -42,7 +42,7 @@ def send_email(rm):
 
     #http://13.69.151.247/05/357105/5/check-nova/Nova-ML2-Sriov/46b5418/env/environment.txt.gz
     fp.write('More details in http://%s/%s\n' % (os.environ.get('EXT_SERVER','') ,os.environ.get('LOG_PATH','')))
-    fp.write("%s\n" % known_issues[rm]['msg'])
+    fp.write("\n%s\n\n" % known_issues[rm]['msg'])
     #fp.write("\n\n\n %s\n" % os.environ)
     fp.write("cloudx-16-01:/home/CI/scripts/rerun_jobs.py -p %s -j %s\n" % (os.environ.get('JOB_NAME',""), os.environ.get('BUILD_NUMBER',"")))
     fp.seek(0)
