20124f08a0abafea3243bbfea204cd775806b3c8
Lenny Verkhovsky <lennyb@mellanox.com>
2018-03-12 12:17:57 +0000

    Updated jobs, fixed testr
    removed stable/ocata added stable/queens
    sriov node rename
    commented simx jobs
    updated repos to external
    testr changed to stestr
    updated cinder
    updated nvme
    fixed backup job
    added from_backup script
    
    Change-Id: Ifc92be10538155a9c67e6014ece52467de989ec5

---
+++ tools/from_backup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+folder=$1
+URL='http://52.169.200.208/BACKUP/'
+
+if [ -z "$folder" ];then
+    folder=$(ls -ltr $nfs | tail -n1|awk '{print $9}')
+fi
+
+echo "Extracting from extracting from $URL/$folder"
+
+cd /tmp
+wget ${URL}/${folder}.gz
+tar -zxvf ${folder}.gz
+cd -
+cp /tmp/${folder}/etc/jobs/* jenkins/jobs/
+cp /tmp/${folder}/etc/zuul/layout/layout.yaml zuul/
+sudo rm -rf /tmp/${folder}*
+
